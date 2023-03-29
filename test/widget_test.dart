b6450c5eb7892ed7499542ce0d3d240fd9f4f95c
erase-huz <125708683+erase-huz@users.noreply.github.com>
2023-03-30 02:40:43 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,23 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:web_view_app/main.dart';
+import 'package:nock/nock.dart';
+import 'package:widget_test/snippet.dart';
 
 void main() {
+  setUpAll(nock.init);
+
+  setUp(() {
+    nock.cleanAll();
+  });
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    nock('https://run.mocky.io')
+        .get('/v3/49c23ebc-c107-4dae-b1c6-5d325b8f8b58')
+      .reply(200, json.encode('{"id": "49c23ebc-c107-4dae-b1c6-5d325b8f8b58", "name": "Example campus" }'));    
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
@@ -27,4 +37,17 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+
+class TestApp extends StatelessWidget {
+  final Widget child;
+
+  TestApp(this.child);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: child,
+    );
+  }
 }
+
