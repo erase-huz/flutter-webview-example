d869aae95ac01bceb930fbc9b01ec865b98a356e
erase-huz <125708683+erase-huz@users.noreply.github.com>
2023-04-04 15:28:16 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ak Rubber',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ItemListPage(title: 'Item List'),
+      home: ItemListPage(title: 'Ak Rubber Industry'),
     );
   }
 }
